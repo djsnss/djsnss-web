f92b730ee605d45aed851921c391b74abd2081e3
anshshah23 <ANSH.SHAH082@svkmmumbai.onmicrosoft.com>
2024-11-20 13:22:44 +0530

    No scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import React, { Suspense } from "react"
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loader from "./components/Loaders/loader2/CustomLoader2";
-const Home = React.lazy(() => import("./pages/Landing"))
+
+const Home = React.lazy(() => import("./pages/Landing"));
+
 const App = () => {
   return (
-    <>
-      <Router>
-        <div className="flex flex-col justify-between min-h-screen">
-          <Suspense fallback={
-            <div className='min-h-screen flex items-center justify-center bg-gradient-to-tr from-blue-300 to-white'>
+    <Router
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
+      <div className="flex flex-col justify-between min-h-screen">
+        <Suspense
+          fallback={
+            <div className="min-h-screen flex items-center justify-center bg-gradient-to-tr from-blue-300 to-white">
               <Loader className="h-40" />
             </div>
-          }>
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
-          </Suspense>
-        </div>
-      </Router>
-    </>
-  )
-}
+          }
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -9,6 +9,7 @@ const data = [
   { src: DJSLogo, alt: 'DJS Logo', link: 'https://djsce.ac.in/' },
   { src: NSSLogo, alt: 'NSS Logo', link: 'https://nss.gov.in/' },
 ];
+
 const Landing = () => {
   return (
     <div className="flex flex-col">
@@ -21,27 +22,24 @@ const Landing = () => {
         transition={{ duration: 1 }}
       >
         <div className="w-full h-full bg-black bg-opacity-55 flex flex-col items-center justify-center">
-          <div className='flex flex-row justify-evenly items-center'>
-            {
-              data.map((d, idx) => (
-                <a href={d.link} target='_blank' rel='noreferrer' key={idx} className='bg-white bg-opacity-30 rounded-full p-2 flex flex-col md:flex-row'>
-                  <motion.a
-                    key={idx}
-                    href={d.link}
-                    target='_blank'
-                    rel='noreferrer'
-                    initial={{ scale: 0.8, opacity: 0 }}
-                    animate={{ scale: 1, opacity: 1 }}
-                    transition={{ duration: 1.2 }}
-                  >
-                    <img src={d.src} alt={d.alt} className="h-10 md:h-40" />
-                  </motion.a>
-                </a>
-              ))
-            }
+          <div className="flex flex-row justify-evenly items-center">
+            {data.map((d, idx) => (
+              <motion.a
+                key={idx}
+                href={d.link}
+                target="_blank"
+                rel="noreferrer"
+                className="bg-white bg-opacity-30 rounded-full p-2 flex"
+                initial={{ scale: 0.8, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{ duration: 1.2 }}
+              >
+                <img src={d.src} alt={d.alt} className="h-10 md:h-40" />
+              </motion.a>
+            ))}
           </div>
           <motion.h1
-            className="text-xl md:text-5xl sm:text-3xl font-bold mb-10 mx-2 text-center text-white"
+            className="text-xl md:text-5xl sm:text-3xl font-bold mx-2 mt-4 text-center text-white"
             initial={{ y: -50, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 1.5 }}
@@ -54,11 +52,34 @@ const Landing = () => {
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 1.5 }}
           >
-            <div className="w-full border-white mb-5 border-b-4"></div>
+            <div className="w-full border-white my-5 border-b-4"></div>
             FOR YOU, WITH YOU, ALWAYS!
           </motion.p>
         </div>
       </motion.div>
+      <div className="flex flex-col items-center justify-center">
+        <motion.h1
+          className="text-2xl md:text-4xl font-bold my-5"
+          initial={{ y: -50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1.5 }}
+        >
+          About Us
+        </motion.h1>
+        <motion.p
+          className="text-md md:text-2xl text-center my-5"
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1.5 }}
+        >
+          The National Service Scheme (NSS) is an Indian government-sponsored
+          public service program conducted by the Ministry of Youth Affairs and
+          Sports of the Government of India. The NSS unit of Dwarkadas J.
+          Sanghvi College of Engineering was established in 2003. Since then,
+          the unit has been actively participating in various social service
+          activities.
+        </motion.p>
+      </div>
     </div>
   );
 };
