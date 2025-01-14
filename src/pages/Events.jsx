7f35968ecb704334a28a9469bdeee927e196b711
tanishshah <shahtanish207@gmail.com>
2025-01-14 17:05:22 +0530

    Integrated fetch upcoming event data

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GenerateCertificate from "../components/events/GenerateCertificate";
 import LargeEvents from "../components/events/LargeEvents";
 import LocalEvents from "../components/events/LocalEvents";
 import UniversityEvents from "../components/events/UniversityEvents";
 import { MapPin, CalendarDays } from "lucide-react";
-import Pic from "../assets/Core.jpg";
 import {
   CCarousel,
   CCarouselItem,
@@ -13,6 +12,35 @@ import {
 } from "@coreui/react";
 
 const Events = () => {
+  const [upcomingEventsData, setUpcomingEventsData] = useState([]);
+
+  useEffect(()=>{
+    const fetchData = async()=>{
+      const response = await fetch("https://djsnss-web.onrender.com/events/upcoming-events")
+      .then((data)=>data.json())
+      .catch((error)=>{
+        console.log(error.message);
+      })
+
+      const formattedEvents = response.events.map((event) => {
+        const eventDate = new Date(event.date);
+        const formattedDate = eventDate
+          .toLocaleDateString("en-GB", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+          })
+          .split("/")
+          .join("-");
+        return { ...event, date: formattedDate };
+      });
+      
+      setUpcomingEventsData(formattedEvents)
+    };
+    
+    fetchData();
+  }, [] )
+
   return (
     <div className="h-max w-full font-poppins scroll-smooth">
       <h1 className="flex mx-auto my-2 h-max w-full justify-center items-center text-[6vw] text-white">
@@ -24,32 +52,44 @@ const Events = () => {
           Upcoming Events :-
         </h1>
 
-        <CCarousel controls indicators>
-          <CCarouselItem>
-            <CImage
-              className="d-block w-100 rounded-lg h-[50vh] sm:h-[60vh] bg-center object-cover"
-              src={Pic}
-              alt="NSS"
-            />
-            <CCarouselCaption className="text-shadow p-4 bg-white/60 rounded-lg">
-              <h5
-                // onClick={() => navigate(`/eventdetails/${event.slug}`)}
-                className="text-black text-base sm:text-xl font-bold cursor-pointer mb-2"
-              >
-                DJS NSS
-              </h5>
-              <p className="text-black text-xs sm:text-sm">Desc</p>
-              <div className="flex justify-center text-xs sm:text-sm text-black">
-                <MapPin className="w-4 h-4 mr-2" />
-                <span>Loc</span>
-              </div>
-              <div className="mt-2 flex justify-center text-xs sm:text-sm  text-black">
-                <CalendarDays className="w-4 h-4 mr-2" />
-                <span>Date</span>
-              </div>
-            </CCarouselCaption>
-          </CCarouselItem>
-        </CCarousel>
+        {upcomingEventsData.length === 0 ? (
+          <p className="text-2xl md:text-3xl w-full text-center font-bold text-white">
+            No Upcoming Events
+          </p>
+        ) : (
+          <CCarousel  controls={upcomingEventsData.length > 1}
+          indicators={upcomingEventsData.length > 1}
+          interval={upcomingEventsData.length > 1 ? 5000 : false}>
+            {upcomingEventsData.map((event) => (
+              <CCarouselItem key={event._id}>
+                <CImage
+                  className="d-block w-100 rounded-lg h-[50vh] sm:h-[60vh] bg-center object-cover"
+                  src={event.imageURL}
+                  alt={event.name}
+                />
+                <CCarouselCaption className="text-shadow p-4 bg-white/60 rounded-lg">
+                  <h5
+                    // onClick={() => navigate(`/eventdetails/${event.slug}`)}
+                    className="text-black text-base sm:text-xl font-bold cursor-pointer mb-2"
+                  >
+                    {event.name}
+                  </h5>
+                  <p className="text-black text-xs sm:text-sm">
+                    {event.description}
+                  </p>
+                  <div className="flex justify-center text-xs sm:text-sm text-black">
+                    <MapPin className="w-4 h-4 mr-2" />
+                    <span>{event.location}</span>
+                  </div>
+                  <div className="mt-2 flex justify-center text-xs sm:text-sm  text-black">
+                    <CalendarDays className="w-4 h-4 mr-2" />
+                    <span>{event.date}</span>
+                  </div>
+                </CCarouselCaption>
+              </CCarouselItem>
+            ))}
+          </CCarousel>
+        )}
       </div>
 
       <LargeEvents />
