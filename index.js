f19ed0cf4d117455c79a830a161e6a2ba531c7e3
Yash Manek <yashmanek2001@gmail.com>
2025-02-07 17:03:48 +0530

    added server limit

--- index.js
+++ index.js
@@ -22,9 +22,8 @@ app.use(
 
 app.options("*", cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json({ limit: "10mb" })); 
+app.use(express.urlencoded({ extended: true, limit: "10mb" })); 
 app.use("/admin", adminRouter);
 app.use("/volunteer", volunteerRouter);
 app.use("/events", eventRouter);
