714c603ed5d447d4356c6ab6f11d7a389ddbe4be
djsnss <djnss.official@gmail.com>
2025-07-16 21:50:42 +0530

    Solved build error

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ import Loader from "./components/Loaders/CustomLoader2";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
-import ScrollTT from "./components/ScrollTT";
 import ChatBot from "./components/ChatBot";
+import ScrollTT from "./components/ScrollTT";
 import { propsData } from "./data/dynamicPageData"; // Import data
 import DynamicPage from "./pages/ThemedPages"; // Import DynamicPage
 import { Toaster } from "react-hot-toast";
