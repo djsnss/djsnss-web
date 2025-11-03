748d37c0ed84ec7899a78e77c91ba43d536f2432
djsnss <djnss.official@gmail.com>
2025-11-03 09:51:10 +0530

    alumni changes

--- src/pages/alumni/AlumniDetails.jsx
+++ src/pages/alumni/AlumniDetails.jsx
@@ -15,36 +15,41 @@ function AlumniDetails() {
   }
 
   return (
-    <div className="flex flex-row md:flex-col items-center justify-center p-6 md:p-10 bg-secondary-blue min-h-screen">
-      <div className="flex flex-row gap-2 pb-10 items-center md:items-start md:mr-10">
+    <div className="min-h-screen bg-secondary-blue flex flex-col items-center justify-start mx-auto pt-20">
+      <div className="flex flex-col sm:flex-row items-center justify-center gap-6 sm:gap-10 max-w-5xl w-full">
+        {/* Image */}
         <img
           src={alumniDetail.image}
           alt={alumniDetail.name}
-          className="w-40 md:w-64 object-cover rounded-xl shadow-2xl border border-blue-600"
+          className="w-48 h-48 sm:w-64 sm:h-64 object-cover rounded-xl shadow-2xl border-2 border-tertiary-blue flex-shrink-0"
         />
-        <div className="flex flex-col pl-20">
-          <h1 className="text-2xl md:text-4xl font-bold font-geist mt-4 md:mt-6 text-gray-800 text-center md:text-left">
+
+        {/* Text Content Wrapper */}
+        <div className="flex flex-col items-center justify-center sm:items-start text-center sm:text-left w-full sm:w-1/3 px-4">
+          <h1 className="text-3xl sm:text-5xl font-bold font-geist text-gray-800">
             {alumniDetail.name}
           </h1>
-          <p className="text-lg md:text-4xl text-black font-lateef  text-center md:text-left">
+          <p className="text-xl sm:text-2xl text-black font-lateef mt-1">
             {alumniDetail.position}
           </p>
+
           {alumniDetail.linkedin && (
-            <div className="flex justify-center font-roboto md:justify-start mt-8">
+            <div className="mt-6">
               <a
                 href={alumniDetail.linkedin}
                 target="_blank"
                 rel="noreferrer"
-                className="text-blue-600 hover:text-blue-800 underline"
+                className="text-tertiary-blue hover:text-dark-blue underline text-lg"
               >
                 View LinkedIn Profile
               </a>
             </div>
           )}
+
         </div>
       </div>
-      <div className="mt-6 md:mt-0 md:ml-10 max-w-2xl">
-        <p className="text-gray-700 text-left  md:text-left font-roboto leading-relaxed pl-3">
+      <div className="mt-4 w-full flex item-center justify-center">
+        <p className="text-base text-tertiary-blue font-roboto leading-relaxed max-w-2xl px-4 py-2">
           {alumniDetail.details || "No details available."}
         </p>
       </div>
